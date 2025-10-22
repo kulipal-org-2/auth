8d48f25aaf379d62139bef8bcab61e49a850442c
Olusola Olagunju <ogolagunju@gmail.com>
2025-10-22 20:41:12 +0100

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices';
 import { join } from 'path';
 import { ExceptionsFilter } from './filters/exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.GRPC,
-      options: {
-        package: 'auth',
-        protoPath: [join(__dirname, 'proto/auth.proto')],
-        url: process.env.URL,
-      },
+  const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice({
+    transport: Transport.GRPC,
+    options: {
+      package: 'auth',
+      protoPath: [join(__dirname, 'proto/auth.proto')],
+      url: process.env.URL,
     },
-  );
+  });
 
   app.useGlobalFilters(new ExceptionsFilter());
 
   app.enableShutdownHooks();
+  await app.startAllMicroservices();
 
-  await app.listen();
+  await app.listen(5000);
 }
 bootstrap();
