437620d9ae2e629f2ac0b966386c6b33068ba7db
Olusola Olagunju <ogolagunju@gmail.com>
2025-10-22 18:31:58 +0100

    feat: pnpm lock

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
-    AuthModule,
+    // AuthModule,
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
   ],
