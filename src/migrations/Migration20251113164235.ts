e145140c96ec20df31ab787e7dbca38bf91f82a3
AyoGbadamosi <gbadamosiayo16@gmail.com>
2025-11-20 09:22:14 +0100

    minor update

--- src/migrations/Migration20251113164235.ts
+++ src/migrations/Migration20251113164235.ts
@@ -7,7 +7,7 @@ export class Migration20251113164235 extends Migration {
     this.addSql(`create index if not exists "otp_identifier_token_index" on "auth"."otp" ("identifier", "token");`);
 
     this.addSql(`alter table "auth"."user" add column if not exists "is_phone_verified" boolean null default false;`);
-    this.addSql(`alter table "auth"."user" add column if not exists "user_type" varchar(255) not null default 'customer';`);
+    this.addSql(`alter table "auth"."user" add column if not exists "user_type" varchar(255) not null default 'user';`);
     this.addSql(`alter table "auth"."user" alter column "phone_number" type varchar(255) using ("phone_number"::varchar(255));`);
     this.addSql(`alter table "auth"."user" alter column "phone_number" set not null;`);
   }
